5cad1a2a7a65bff9b06b4f38609393b33772e2eb
scazan <scottcazan@gmail.com>
2019-06-17 23:00:03 +0200

    Switch to a router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import SixteenStep from './views/SixteenStep/SixteenStep';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SixteenStep />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <Switch>
+      <Route path="/step" component={ SixteenStep } />
+      <Route path="/" component={ App } />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(
+  routing,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
